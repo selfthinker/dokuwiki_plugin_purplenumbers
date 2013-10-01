f37057c35b8b2b81a1953102d0a926634f9018dc
Aurimas <aurimas.gercas@inbox.lt>
2013-10-01 19:20:13 +0200

    translation update

---
+++ lang/lt/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aurimas <aurimas.gercas@inbox.lt>
+ */
+$lang['numbering']             = 'ID tipas';
+$lang['numbering_o_1']         = 'Herarchija';
+$lang['uniqueness']            = 'Unikalus id`s';
+$lang['linkText']              = 'nuorodos tekstas';
+$lang['linkText_o_0']          = 'paragrafas/sekcija prisijungti';
+$lang['linkText_o_1']          = 'ID';
