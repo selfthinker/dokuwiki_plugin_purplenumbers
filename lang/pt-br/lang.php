28072a50f8ba32d8b8119ba3adb50b5cbca01366
Samory Pereira Santos <samory.santos@gmail.com>
2014-01-06 21:50:13 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,8 +1,9 @@
 <?php
-/**
- * Brazilian Portuguese language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Samory Pereira Santos <samory.santos@gmail.com>
+ */
 $lang['sectionlink']           = 'Link para esta sessão';
+$lang['comment']               = 'comentário';
