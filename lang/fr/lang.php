0cc8ff77987fe4661b4abbf2fca4f996b28540fb
Denis Villeneuve <denis@flashcom.qc.ca>
2014-01-07 20:35:05 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,8 +1,9 @@
 <?php
-/**
- * French language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
 
-$lang['sectionlink']           = 'lien à cette section';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Denis Villeneuve <denis@flashcom.qc.ca>
+ */
+$lang['sectionlink']           = 'lien vers cette section';
+$lang['comment']               = 'commentaire';
