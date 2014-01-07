fd6f827707c049ba981ddf365e57f1ddfce9d115
Denis Villeneuve <denis@flashcom.qc.ca>
2014-01-07 20:40:22 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,8 +1,7 @@
 <?php
-/**
- * French language file
- *
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- */
 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
 $lang['sectionlink']           = 'lien à cette section';
