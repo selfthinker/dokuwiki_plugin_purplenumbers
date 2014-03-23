14aed3df7cdda2ec325349d9fa57ad0e8f06d8ed
Anika Henke <anika@selfthinker.org>
2014-03-23 13:09:32 +0000

    added German lang file for settings

---
+++ lang/de/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * German language file for config of PurpleNumbers plugin
+ *
+ */
+
+$lang['numbering']             = 'Art der ID-Numerierung';
+  $lang['numbering_o_0']       = 'aufeinanderfolgend';
+  $lang['numbering_o_1']       = 'hierarchisch';
+$lang['uniqueness']            = 'Eindeutigkeit der IDs';
+  $lang['uniqueness_o_0']      = 'seitenweit';
+  $lang['uniqueness_o_1']      = 'wikiweit';
+$lang['internalID']            = 'Erzeuge IDs, die als interne Anker verwendet werden können? (Achtung: könnten nicht eindeutig sein)';
+$lang['linkText']              = 'Linkttext';
+  $lang['linkText_o_0']        = 'Paragraphen-/Abschnittsymbol';
+  $lang['linkText_o_1']        = 'ID';
+$lang['restrictionNS']         = 'Beschränke Nutzung des Plugins auf diese (Komma-getrennten) Namensräume erster Ebene (leerlassen, wenn PurpleNumbers immer angezeigt werden sollen)';
+$lang['restrictionType']       = 'Beschränkungsart, bestimmt ob Seiten unter den angegebenen Namensräumen für das Anzeigen von PurpleNumbers eingeschlossen oder ausgeschlossen werden sollen';
+  $lang['restrictionType_o_0'] = 'ausschliessen';
+  $lang['restrictionType_o_1'] = 'einschliessen';
+$lang['includeStartpage']      = 'Zeige PurpleNumbers auf Startseiten?';
+$lang['annotationPage']        = 'Muster für Links zu den Kommentarseiten (leerlassen um diese Links abzuschalten)';
