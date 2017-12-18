3c3efc06150862a2c1c726de9308044b9e649240
Tor Härnqvist <tor@harnqvist.se>
2017-12-18 20:40:39 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['sectionlink']           = 'länk till denna sektion';
+$lang['comment']               = 'kommentar';
---
+++ lang/sv/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['numbering']             = 'typ av ID-numrering';
+$lang['numbering_o_0']         = 'konsekutiva';
+$lang['numbering_o_1']         = 'hierarkisk';
+$lang['linkText']              = 'länktext';
+$lang['linkText_o_0']          = 'stycke-/sektionssymbol';
+$lang['linkText_o_1']          = 'ID';
+$lang['restrictionType_o_0']   = 'exkludera';
+$lang['restrictionType_o_1']   = 'inkludera';
