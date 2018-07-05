2698d82f78e99997de7b1d54f593a23e34f6e712
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:55:15 +0200

    translation update

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -2,9 +2,23 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
+$lang['numbering']             = 'tipo de numeração de ID';
 $lang['numbering_o_0']         = 'consecutivo';
 $lang['numbering_o_1']         = 'hierárquico';
+$lang['uniqueness']            = 'singularidade de IDs';
+$lang['uniqueness_o_0']        = 'page-wide';
+$lang['uniqueness_o_1']        = 'site-wide';
+$lang['internalID']            = 'criar IDs que podem ser usados como âncoras internas? (Atenção: pode não ser único)';
+$lang['linkText']              = 'texto do link';
+$lang['linkText_o_0']          = 'parágrafo/seção';
+$lang['linkText_o_1']          = 'ID';
+$lang['restrictionNS']         = 'restringir o uso do plug-in para esses namespaces de primeiro nível (separados por vírgulas) (deixe em branco se os números roxos sempre serão mostrados)';
+$lang['restrictionType']       = 'tipo de restrição, especifica se as páginas sob os namespaces especificados devem ser incluídas ou excluídas para a exibição de números roxos';
+$lang['restrictionType_o_0']   = 'excluir';
+$lang['restrictionType_o_1']   = 'incluir';
 $lang['includeStartpage']      = 'mostrar Purple Number nas páginas iniciais?';
+$lang['annotationPage']        = 'padrão para links para páginas de anotação (deixe em branco para desativar)';
