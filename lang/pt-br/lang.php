6e01c463e26ae2ef95ada8ebe75a34a78e56885e
Zeytinburnu Nakliyat <ralfirby@yahoo.com.br>
2023-12-27 12:45:18 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Zeytinburnu Nakliyat <ralfirby@yahoo.com.br>
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
-$lang['sectionlink']           = 'Link para esta sessão';
-$lang['comment']               = 'comentário';
+$lang['sectionlink']           = 'https://evigetir.com/Zeytinburnu-Nakliyat-om-za';
+$lang['comment']               = 'Hi there to all, how is all, I think every one is getting more from this site, and your views 
+are pleasant for new users.';
--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -3,22 +3,35 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Zeytinburnu Nakliyat <ralfirby@yahoo.com.br>
  * @author Alexandre Belchior <alexbelchior@gmail.com>
  * @author Samory Pereira Santos <samory.santos@gmail.com>
  */
-$lang['numbering']             = 'tipo de numeração de ID';
-$lang['numbering_o_0']         = 'consecutivo';
-$lang['numbering_o_1']         = 'hierárquico';
-$lang['uniqueness']            = 'singularidade de IDs';
-$lang['uniqueness_o_0']        = 'page-wide';
-$lang['uniqueness_o_1']        = 'site-wide';
-$lang['internalID']            = 'criar IDs que podem ser usados como âncoras internas? (Atenção: pode não ser único)';
-$lang['linkText']              = 'texto do link';
-$lang['linkText_o_0']          = 'parágrafo/seção';
-$lang['linkText_o_1']          = 'ID';
-$lang['restrictionNS']         = 'restringir o uso do plug-in para esses namespaces de primeiro nível (separados por vírgulas) (deixe em branco se os números roxos sempre serão mostrados)';
-$lang['restrictionType']       = 'tipo de restrição, especifica se as páginas sob os namespaces especificados devem ser incluídas ou excluídas para a exibição de números roxos';
-$lang['restrictionType_o_0']   = 'excluir';
-$lang['restrictionType_o_1']   = 'incluir';
-$lang['includeStartpage']      = 'mostrar Purple Number nas páginas iniciais?';
-$lang['annotationPage']        = 'padrão para links para páginas de anotação (deixe em branco para desativar)';
+$lang['numbering']             = 'Hi there to all, how is all, I think every one is getting more from 
+this site, and your views are pleasant for new users.';
+$lang['numbering_o_0']         = 'Hi there to all, how is all, I think every one is getting more from this 
+site, and your views are pleasant for new users.';
+$lang['numbering_o_1']         = 'Hi there to all, how is all, I think every one is getting more from this 
+site, and your views are pleasant for new users.';
+$lang['uniqueness']            = 'Hi there to all, how is all, I think every one is getting more from this site, and your views are pleasant for new users.';
+$lang['uniqueness_o_0']        = 'Hi there to all, how is all, I think every one is getting more from this 
+site, and your views are pleasant for new users.';
+$lang['uniqueness_o_1']        = 'https://evigetir.com/Zeytinburnu-Nakliyat-om-za';
+$lang['internalID']            = 'Hi there to all, how is all, I think every one 
+is getting more from this site, and your views are pleasant for new 
+users.';
+$lang['linkText']              = 'https://evigetir.com/Zeytinburnu-Nakliyat-om-za';
+$lang['linkText_o_0']          = 'Hi there to all, how is all, I think every one is getting more from 
+this site, and your views are pleasant for new users.';
+$lang['linkText_o_1']          = 'Hi there to all, how is all, I think every one is getting more from this site, and your views are pleasant for new users.';
+$lang['restrictionNS']         = 'Ralf';
+$lang['restrictionType']       = 'Ralf';
+$lang['restrictionType_o_0']   = 'Hi there to all, how is all, I think every one is getting more from 
+this site, and your views are pleasant for new users.';
+$lang['restrictionType_o_1']   = 'Hi there to all, how is all, I think every one is getting 
+more from this site, and your views are pleasant for new users.';
+$lang['includeStartpage']      = 'Hi there to all, how is all, I think every one 
+is getting more from this site, and your views are pleasant for new users.';
+$lang['annotationPage']        = 'Hi there to all, how is all, I think every one 
+is getting more from this site, and your views are pleasant for 
+new users.';
